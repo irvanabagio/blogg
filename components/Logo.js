38017dc9f7be22d250c3d9a8d09c5e623ece92be
anonymoussc <50c5ac69@opayq.com>
2018-01-21 12:41:30 +0700

    Modified Logo.js components

--- components/Logo.js
+++ components/Logo.js
@@ -5,12 +5,18 @@ import {colors, fonts} from 'css';
 
 function Logo({}) {
     return (
+        // <Link to={prefixLink('/')} style={{
+        //     color         : colors.black,
+        //     textDecoration: 'none',
+        //     textTransform : 'uppercase',
+        //     fontFamily    : fonts.secondary,
+        //     fontWeight    : fonts.bold,
+        // }}>
         <Link to={prefixLink('/')} style={{
             color         : colors.black,
             textDecoration: 'none',
             textTransform : 'uppercase',
-            fontFamily    : fonts.secondary,
-            fontWeight    : fonts.bold,
+            fontFamily    : fonts.secondary
         }}>
             RN <span style={{fontWeight: fonts.xThin, marginLeft: 5,}}>Newsletter</span>
         </Link>
